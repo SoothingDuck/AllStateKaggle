d4bea18c15f6c679cec18b3cb0270f3267ef1a14
Yvan Aillet <yvanaillet@yahoo.fr>
2014-03-05 14:45:39 +0100

    33%

--- first_model.R
+++ first_model.R
@@ -60,7 +60,34 @@ trainIndex <- createDataPartition(data$last_A, p = .8,
 dataTrain <- data[trainIndex,]
 dataTest <- data[-trainIndex,]
 
+
+# Estimation modeles
+prediction_error <- function(true_data, predicted_data) {
+  
+  ok_prediction <- sum(true_data == predicted_data)
+  ko_prediction <- sum(true_data != predicted_data)
+  
+  return ((ko_prediction)/(ok_prediction+ko_prediction))
+}
+
+list_prob <- seq(.2, .8, .1)
+
+list_prob <- c(.8)
+
+result <- data.frame()
+
+for(prob in list_prob) {
+  
+cat("Taille train : ", prob, "\n")
+
+trainIndex <- createDataPartition(data$last_A, p = prob,
+                                  list = FALSE,
+                                  times = 1)
+
+dataTrain <- data[trainIndex,]
+  
 # Evaluation modeles
+
 model_0 <- glm(
   I(last_A == "0") ~ . 
   + I(first_day == last_day)
@@ -76,26 +103,40 @@ model_2 <- glm(
   + I(first_day == last_day)
   , family = binomial, data=dataTrain)
 
-model_rf <- randomForest(
-  last_A ~ . - state
-  + I(first_day == last_day),
-  data=dataTrain,
-  importance=TRUE,
-  do.trace=TRUE
-  )
+# model_rf <- randomForest(
+#   last_A ~ . - state
+#   + I(first_day == last_day),
+#   data=dataTrain,
+#   importance=TRUE,
+#   do.trace=TRUE,
+#   ntree=20
+#   )
 
 dataTest$predict_glm_0 <- predict(model_0, newdata=dataTest)
 dataTest$predict_glm_1 <- predict(model_1, newdata=dataTest)
 dataTest$predict_glm_2 <- predict(model_2, newdata=dataTest)
 
 dataTest$predicted_glm_A <- factor(max.col(dataTest[,c("predict_glm_0","predict_glm_1", "predict_glm_2")])-1)
+# dataTest$predicted_rf_A <- predict(model_rf, newdata=dataTest)
 
-prediction_error <- function(true_data, predicted_data) {
-  
-  ok_prediction <- sum(true_data == predicted_data)
-  ko_prediction <- sum(true_data != predicted_data)
-  
-  return ((ko_prediction)/(ok_prediction+ko_prediction))
-}
+dataTrain$predict_glm_0 <- predict(model_0, newdata=dataTrain)
+dataTrain$predict_glm_1 <- predict(model_1, newdata=dataTrain)
+dataTrain$predict_glm_2 <- predict(model_2, newdata=dataTrain)
 
+dataTrain$predicted_glm_A <- factor(max.col(dataTrain[,c("predict_glm_0","predict_glm_1", "predict_glm_2")])-1)
+
+
+print("Error GLM :")
 print(prediction_error(dataTest$last_A, dataTest$predicted_glm_A))
+
+# print("Error RF :")
+# print(prediction_error(dataTest$last_A, dataTest$predicted_rf_A))
+
+result <- rbind(result, data.frame(
+  size.train=prob, 
+  error.glm.test=prediction_error(dataTest$last_A, dataTest$predicted_glm_A),
+  error.glm.train=prediction_error(dataTrain$last_A, dataTrain$predicted_glm_A)
+)
+)
+  
+}
