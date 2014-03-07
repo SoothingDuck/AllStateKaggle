f365a48e64760b443219ca110e2aa7408850f7fe
Yvan Aillet <yvanaillet@yahoo.fr>
2014-03-07 15:22:00 +0100

    ajout recup Test

--- get_data.R
+++ get_data.R
@@ -1,6 +1,212 @@
 library(RSQLite)
 library(stringr)
 
+
+get.data.test <- function() {
+  sqlitedb.filename <- "allstate_data.sqlite3"
+  
+  drv <- dbDriver("SQLite")
+  con <- dbConnect(drv, dbname=sqlitedb.filename)
+  
+  data <- dbGetQuery(con,
+                     "
+  select 
+  T1.customer_ID,
+  T3.state,
+  T1.day as first_view_day,
+  T7.day as last_view_day,
+  T9.day as min_cost_view_day,
+  T1.time as first_view_time,
+  T7.time as last_view_time,
+  T9.time as min_cost_view_time,
+  T4.nb_views,
+  -- T1.location as first_view_location,
+  case
+    when T5.location_popularity is null then 0
+    else T5.location_popularity
+  end as location_popularity,
+  T4.nb_distinct_location,
+  T1.group_size as first_view_group_size,
+  T7.group_size as last_view_group_size,
+  T9.group_size as min_cost_view_group_size,
+  T1.homeowner as first_view_homeowner,
+  T7.homeowner as last_view_homeowner,
+  T9.homeowner as min_cost_view_homeowner,
+  T1.car_age as first_view_car_age,
+  T7.car_age as last_view_car_age,
+  T9.car_age as min_cost_view_car_age,
+  T1.car_value as first_view_car_value,
+  T7.car_value as last_view_car_value,
+  T9.car_value as min_cost_view_car_value,
+  case
+  when T1.risk_factor is null then 'Not available'
+  else T1.risk_factor || ''
+  end as first_view_risk_factor,
+  case
+  when T7.risk_factor is null then 'Not available'
+  else T7.risk_factor || ''
+  end as last_view_risk_factor,
+  case
+  when T9.risk_factor is null then 'Not available'
+  else T9.risk_factor || ''
+  end as min_cost_view_risk_factor,
+  T1.age_oldest as first_view_age_oldest,
+  T7.age_oldest as last_view_age_oldest,
+  T9.age_oldest as min_cost_view_age_oldest,
+  T1.age_youngest as first_view_age_youngest,
+  T7.age_youngest as last_view_age_youngest,
+  T9.age_youngest as min_cost_view_age_youngest,
+  T1.married_couple as first_view_married_couple,
+  T7.married_couple as last_view_married_couple,
+  T9.married_couple as min_cost_view_married_couple,
+  case
+  when T1.C_previous is null then 'Not available'
+  else T1.C_previous || ''
+  end as first_view_C_previous,
+  case
+  when T7.C_previous is null then 'Not available'
+  else T7.C_previous || ''
+  end as last_view_C_previous,
+  case
+  when T9.C_previous is null then 'Not available'
+  else T9.C_previous || ''
+  end as min_cost_view_C_previous,
+  case
+  when T1.duration_previous is null then 'Not available'
+  else T1.duration_previous || ''
+  end as first_view_duration_previous,
+  case
+  when T7.duration_previous is null then 'Not available'
+  else T7.duration_previous || ''
+  end as last_view_duration_previous,
+  case
+  when T9.duration_previous is null then 'Not available'
+  else T9.duration_previous || ''
+  end as min_cost_view_duration_previous,
+  T1.cost as first_view_cost,
+  T7.cost as last_view_cost,
+  T9.cost as min_cost_view_cost,
+  T1.A as first_view_A,
+  T7.A as last_view_A,
+  T9.A as min_cost_view_A,
+  T1.B as first_view_B,
+  T7.B as last_view_B,
+  T9.B as min_cost_view_B,
+  T1.C as first_view_C,
+  T7.C as last_view_C,
+  T9.C as min_cost_view_C,
+  T1.D as first_view_D,
+  T7.D as last_view_D,
+  T9.D as min_cost_view_D,
+  T1.E as first_view_E,
+  T7.E as last_view_E,
+  T9.E as min_cost_view_E,
+  T1.F as first_view_F,
+  T7.F as last_view_F,
+  T9.F as min_cost_view_F,
+  T1.G as first_view_G,
+  T7.G as last_view_G,
+  T9.G as min_cost_view_G
+  from
+  transactions T1 left outer join
+  (
+  select
+  location,
+  count(distinct customer_ID) as location_popularity
+  from
+  transactions
+  group by 1
+  ) T5 on T1.location = T5.location
+  , customers T3,
+  (
+  select
+  customer_ID,
+  count(*) as nb_views,
+  count(distinct location) as nb_distinct_location
+  from
+  transactions
+  where
+  record_type = 0
+  group by 1
+  ) T4,
+  (
+  select
+  customer_ID,
+  max(shopping_pt) as last_view_shopping_pt
+  from
+  transactions
+  group by 1
+  ) T6,
+  transactions T7,
+  (
+  select
+  A.customer_ID,
+  max(B.shopping_pt) as min_cost_shopping_pt
+  from
+  (
+  select
+  customer_ID,
+  min(cost) as min_cost
+  from transactions
+  where
+  record_type = 0
+  group by 1
+  ) A,
+  (
+  select
+  customer_ID,
+  cost,
+  shopping_pt
+  from transactions
+  where
+  record_type = 0
+  ) B,
+  customers C
+  where
+  A.customer_ID = C.customer_ID
+  and
+  C.dataset = 'test'
+  and
+  A.customer_ID = B.customer_ID
+  and
+  A.min_cost = B.cost
+  group by 1
+  ) T8,
+  transactions T9
+  where
+  T1.customer_ID = T8.customer_ID
+  and
+  T8.customer_ID = T9.customer_ID
+  and
+  T8.min_cost_shopping_pt = T9.shopping_pt
+  and
+  T1.customer_ID = T6.customer_ID
+  and
+  T6.customer_ID = T7.customer_ID
+  and
+  T6.last_view_shopping_pt = T7.shopping_pt
+  --and
+  --T1.location = T5.location
+  and
+  T1.customer_ID = T4.customer_ID
+  --and
+  --T1.customer_ID = T2.customer_ID
+  and
+  T1.line_number = 1
+  --and
+  --T2.record_type = 1
+  and
+  T3.dataset = 'test'
+  and
+  T1.customer_ID = T3.customer_ID
+                     ")
+  
+  dbDisconnect(con)
+
+  return(data)
+}
+
+
 get.data.train <- function() {
   sqlitedb.filename <- "allstate_data.sqlite3"
   
@@ -13,7 +219,7 @@ get.data.train <- function() {
   T1.customer_ID,
   T3.state,
   T1.day as first_view_day,
-  T2.day as last_view_day,
+  T7.day as last_view_day,
   T9.day as min_cost_view_day,
   T1.time as first_view_time,
   T7.time as last_view_time,
@@ -315,6 +521,14 @@ normalize.train.data <- function(data) {
   data$real_G <- factor(data$real_G)
   
   return(data)
+}
+
+normalize.test.data <- function(data) {
+  
+  data <- normalize.data(data)
+  
+  return(data)
+  
 }
 
 # Préparation des données
