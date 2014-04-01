732d867d917677518c6cae85f29249d2256cadfc
Yvan Aillet <yvanaillet@yahoo.fr>
2014-04-01 22:04:35 +0200

    glm cluster 2 et 3

---
+++ models/glm_model_cluster_3/generate_model_cluster_3.R
@@ -0,0 +1,28 @@
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_A.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_B.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_C.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_D.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_E.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_F.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_cluster_3", "glm_cluster_3_model_G.R"))
+rm(list=ls())
+gc(TRUE)
