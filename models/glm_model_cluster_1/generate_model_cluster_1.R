ab770636eef0a9d914f24d81bd376b9f859b1eca
Yvan Aillet <yvanaillet@yahoo.fr>
2014-04-01 15:13:05 +0200

    fichiers de base

---
+++ models/glm_model_cluster_1/generate_model_cluster_1.R
@@ -0,0 +1,28 @@
+
+source(file.path("models", "glm_model_no_clusters", "first_model_A.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_no_clusters", "first_model_B.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_no_clusters", "first_model_C.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_no_clusters", "first_model_D.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_no_clusters", "first_model_E.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_no_clusters", "first_model_F.R"))
+rm(list=ls())
+gc(TRUE)
+
+source(file.path("models", "glm_model_no_clusters", "first_model_G.R"))
+rm(list=ls())
+gc(TRUE)
