aff4965d12bee7531fb5b320138622ef624c9937
Yvan Aillet <yvanaillet@yahoo.fr>
2014-04-07 17:57:36 +0200

    ggplot

--- check_state_G.R
+++ check_state_G.R
@@ -3,10 +3,9 @@
 source(file.path("templates","functions.R"))
 source(file.path("templates","get_data.R"))
 
-stop("to finish")
 
 # Models
-load(file=file.path("DATA", "OUTPUT", ""))
+load(file=file.path("DATA", "OUTPUT", "first_model_glm_all_clusters_G.RData"))
                     
 # Selection variable à estimer
 set.seed(42)
@@ -16,5 +15,17 @@ dataTrainBase <- tmp$train
 dataTest <- tmp$test
 
 
+dataTrainBase$predicted_G_1 <- predict(model_1_final_G, newdata=dataTrainBase)
+dataTrainBase$predicted_G_2 <- predict(model_2_final_G, newdata=dataTrainBase)
+dataTrainBase$predicted_G_3 <- predict(model_3_final_G, newdata=dataTrainBase)
+dataTrainBase$predicted_G_4 <- predict(model_4_final_G, newdata=dataTrainBase)
+dataTrainBase$customer_ID <- rownames(dataTrainBase)
 
-source(file.path("templates","glm_skeleton_final_training_G.R"))
+# Check
+library(ggplot2)
+library(plyr)
+library(reshape2)
+
+m <- melt(dataTrainBase, id.vars=c("customer_ID","state","real_G"), measure.vars=c("predicted_G_1","predicted_G_2","predicted_G_3","predicted_G_4"))
+
+ggplot(subset(m, real_G == "1")) + geom_histogram(aes(x=value)) + facet_grid(state + real_G  ~ variable )
