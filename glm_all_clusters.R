6e17a5927aa42306cc83e6fcc790987f5ccf09db
Yvan Aillet <yvanaillet@yahoo.fr>
2014-04-10 00:02:06 +0200

    stopping clusters

--- glm_all_clusters.R
+++
@@ -1,4 +0,0 @@
-source(file.path("models", "glm_model_cluster_1", "generate_model_cluster_1.R"))
-source(file.path("models", "glm_model_cluster_2", "generate_model_cluster_2.R"))
-source(file.path("models", "glm_model_cluster_3", "generate_model_cluster_3.R"))
-
