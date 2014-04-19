1b6a6e9a198ccc4d8ef8f24bf4c4861f41b9bb99
Yvan Aillet <yvanaillet@yahoo.fr>
2014-04-19 18:20:10 +0200

    ajout C

--- models/glm_model_all_clusters/generate_model_all_clusters.R
+++ models/glm_model_all_clusters/generate_model_all_clusters.R
@@ -4,17 +4,19 @@
 # source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_with_risk_factor_A.R"))
 # source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_without_risk_factor_A.R"))
 
-# model B
-source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_with_location_with_risk_factor_B.R"))
-source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_with_location_without_risk_factor_B.R"))
-source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_with_risk_factor_B.R"))
-source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_without_risk_factor_B.R"))
+# # model B
+# source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_with_location_with_risk_factor_B.R"))
+# source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_with_location_without_risk_factor_B.R"))
+# source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_with_risk_factor_B.R"))
+# source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_without_risk_factor_B.R"))
+
+# model C
+source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_with_location_with_risk_factor_C.R"))
+source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_with_location_without_risk_factor_C.R"))
+source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_with_risk_factor_C.R"))
+source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_without_location_without_risk_factor_C.R"))
 
 stop()
-
-source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_C.R"))
-rm(list=ls())
-gc(TRUE)
 
 source(file.path("models", "glm_model_all_clusters", "glm_all_clusters_model_D.R"))
 rm(list=ls())
