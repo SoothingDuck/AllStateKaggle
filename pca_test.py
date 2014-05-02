546ab1a3d0262d9a7a8450e652b6825eba3f3a21
Yvan Aillet <yvanaillet@yahoo.fr>
2014-05-02 08:30:54 +0200

    CD et CE

--- pca_test.py
+++ pca_test.py
@@ -57,8 +57,8 @@ def duplicate_data(data):
     for letter_1 in ['A','B','C','D','E','F','G']:
         for letter_2 in ['A','B','C','D','E','F','G']:
             if letter_1 < letter_2:
-                for variable_1 in [x for x in list(tmp.columns) if x.startswith("value_%s_pt_2" % letter_1)]:
-                    for variable_2 in [x for x in list(tmp.columns) if x.startswith("value_%s_pt_2" % letter_2)]:
+                for variable_1 in [x for x in list(tmp.columns) if x.startswith("value_%s_last" % letter_1)]:
+                    for variable_2 in [x for x in list(tmp.columns) if x.startswith("value_%s_last" % letter_2)]:
                         print "Eval %s => %s" % (variable_1, variable_2)
                         variable_12 = "dup_%s_%s" % (variable_1, variable_2)
                         tmp[variable_12] = tmp.apply(concat_letter_12, axis=1)
@@ -78,14 +78,27 @@ data_3 = l.get_data_3_train()
 print("Extraction data_all...")
 data_all = l.get_data_all_train()
 
-data_2_reindexed = duplicate_data(data_2)
+data_all_reindexed = duplicate_data(data_all)
 
-pca = decomposition.PCA(n_components=2, whiten=False)
+pca = decomposition.SparsePCA(n_components=3, verbose=True)
 
-X = get_X_without_scaler(data_2_reindexed)
+X = get_X_without_scaler(data_all_reindexed)
 pca.fit(X)
 X_pca = pca.transform(X)
 
-plt.plot(X_pca[data_all["real_A"] == 1,0], X_pca[data_all["real_A"] == 1,1], "b+")
-plt.plot(X_pca[data_all["real_A"] == 2,0], X_pca[data_all["real_A"] == 2,1], "g+")
-plt.plot(X_pca[data_all["real_A"] == 0,0], X_pca[data_all["real_A"] == 0,1], "r+")
+x1 = -600
+x2 = 400
+y1 = -80
+y2 = 80
+
+plt.figure()
+plt.subplot(311)
+plt.plot(X_pca[data_all["real_A"] == 1,2], X_pca[data_all["real_A"] == 1,1], "b+")
+#plt.axis((x1,x2,y1,y2))
+plt.subplot(312)
+plt.plot(X_pca[data_all["real_A"] == 2,2], X_pca[data_all["real_A"] == 2,1], "g+")
+#plt.axis((x1,x2,y1,y2))
+plt.subplot(313)
+plt.plot(X_pca[data_all["real_A"] == 0,2], X_pca[data_all["real_A"] == 0,1], "r+")
+#plt.axis((x1,x2,y1,y2))
+plt.show()
